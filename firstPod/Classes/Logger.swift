221016280e9bb41265101f13e4e63935b9d632b9
sara <sara.salar910@gmail.com>
2022-12-20 13:24:55 +0000

    first commit

---
+++ firstPod/Classes/Logger.swift
@@ -0,0 +1,21 @@
+//
+//  Logger.swift
+//  firstPod
+//
+//  Created by Sara on 11/12/2022.
+//
+
+import Foundation
+
+public class Logger{
+   public init(){}
+    
+    public func printLog(){
+        print("Hello World")
+        
+    }
+    
+    func privateMethod(){
+        print("Private")
+    }
+}
